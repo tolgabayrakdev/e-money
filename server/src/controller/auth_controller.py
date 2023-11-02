c637b092cc5891442578517903381e33ab62dd09
tolgabayrakdev <bayraktolga28@gmail.com>
2023-11-02 19:53:13 +0300

    test

--- server/src/controller/auth_controller.py
+++ server/src/controller/auth_controller.py
@@ -31,14 +31,17 @@ async def logout(response: Response):
 
 @auth_router.post("/verify", status_code=200)
 async def verify_user(request: Request):
-    auth_header = request.cookies.get("access_token") and request.cookies.get("refresh_token")
-    if auth_header is not None:
-        decoded_token = jwt.decode(auth_header, "secret_key", algorithms=["HS256"])
-        id = decoded_token["payload"]["user_id"]
-        result = AuthService.user_information(id)
-        return {"success": True, "user": {
-            "username": result.username,
-            "email": result.email
-        }}
-    else:
-        raise HTTPException(status_code=401, detail="Unauthorization")
+    try:
+        auth_header = request.cookies.get("access_token") and request.cookies.get("refresh_token")
+        if auth_header is not None:
+            decoded_token = jwt.decode(auth_header, "secret_key", algorithms=["HS256"])
+            id = decoded_token["payload"]["user_id"]
+            result = AuthService.user_information(id)
+            return {"success": True, "user": {
+                "username": result.username,
+                "email": result.email
+            }}
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorization")
+    except jwt.ExpiredSignatureError as e:
+        raise HTTPException(status_code=403, detail=str(e))
