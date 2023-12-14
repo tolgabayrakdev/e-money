53080de9de90fcb28ffee3d415faa44b32b463ad
tolgabayrakdev <bayraktolga28@gmail.com>
2023-12-14 20:38:29 +0300

    forget password page email state

--- client/src/app/(authentication)/reset-password/page.tsx
+++ client/src/app/(authentication)/reset-password/page.tsx
@@ -1,8 +1,10 @@
 import { Button, Card, CardBody, CardFooter, Heading, Input, Stack } from '@chakra-ui/react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ResetPassword() {
+  const [email, setEmail] = useState("");
+
   return (
     <div>
       <Card width="64" bgColor="white" borderColor="teal">
@@ -10,8 +12,7 @@ export default function ResetPassword() {
           <CardBody>
             <Stack mt='6' spacing='3'>
               <Heading className="text-center" color="dark" size='md'>Reset password</Heading>
-              <Input borderColor="teal.600" focusBorderColor="teal.600" placeholder='Enter email' />
-
+              <Input onChange={(e)=>setEmail(e.target.value)} borderColor="teal.600" focusBorderColor="teal.600" placeholder='Enter email' />
             </Stack>
           </CardBody>
           <Link className="block text-xs ml-5 hover:underline" href="login">Back to login</Link>
